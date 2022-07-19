a748c33833e9a5dc8c46130484ed0684fd522872
Afnan <Afnan@DESKTOP-7MLJB30>
2022-07-19 11:44:03 +0530

    SASSA

---
+++ mvnproject/src/test/java/mvnproject/practicemvn.java
@@ -0,0 +1,37 @@
+package mvnproject;
+
+import org.testng.annotations.Test;
+
+public class practicemvn {
+	@Test
+	public void test() {
+		
+		String URL =System.getProperty("URL");
+		String Browser =System.getProperty("Browser");
+		String userName =System.getProperty("UN");
+		String Password =System.getProperty("PWD");
+		
+		System.out.println("ADNAN");
+		System.out.println("URL =="+URL);
+		System.out.println("Browser =="+Browser);
+		System.out.println("UN =="+userName);
+		System.out.println("PWD =="+Password);
+
+	}
+	@Test(priority = 3)
+	public void test1() {
+		System.out.println("tyss02");
+
+	}
+	@Test
+	public void test2() {
+		System.out.println("tyss03");
+
+	}
+	@Test(enabled=false)
+	public void test3() {
+		System.out.println("tyss04");
+
+	}
+}
+
